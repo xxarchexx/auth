74a62ce5578c16894a580a06b758d29c13ab3414
xxarchexx <sansolovyov@mail.ru>
2019-07-17 03:01:52 -0400

    test

--- mail/manager.go
+++ mail/manager.go
@@ -17,12 +17,12 @@ type message struct {
 //SendMessage smpt message
 func SendMessage(to, body, subject string) {
 	m := gomail.NewMessage()
-	m.SetHeader("From", "sansolovyov19866@gmail.com")
-	m.SetHeader("To", "sansolovyov@mail.ru")
+	m.SetHeader("From", "22@gmail.com")
+	m.SetHeader("To", "22@mail.ru")
 	// m.SetAddressHeader("Cc", "")
 	m.SetBody("text/html", body)
-	d := gomail.NewDialer("smtp.gmail.com", 25, "sansolovyov19866@gmail.com", "Alexander14a@93dy")
-	err := d.DialAndSend(m)
+	d := gomail.NewDialer("smtp.11.com", 25, "1@gmail.com", "1@93dy")
+	err := d.DialAndSend(m)2
 	if err != nil {
 		panic(err)
 	}
