1ba48a8d9ccb950248fc3ea6f62c24f28461ed89
xxarchexx <sansolovyov@mail.ru>
2020-01-08 08:47:27 -0500

    Nice Looking Login Page

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
 import jwtDecode from 'jwt-decode';
 import { BrowserRouter, Route } from 'react-router-dom';
-import App from './rootContainers/App';
-import Greetings from './components/Greetings';
-import SignupPage from './components/signup/SignupPage';
-import LoginSignUpLinkPage from './rootContainers/LoginSignUpLinkPage';
-
+// import App from './rootContainers/App';
+// import Greetings from './components/Greetings';
+// import SignupPage from './components/signup/SignupPage';
+// import SignIn from './components/login';
+import AuthForm from './log/App'
 const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null;
 
 const store = createStore(
@@ -28,10 +28,10 @@ if (localStorage.jwtToken) {
 render(
   <Provider store={store}>
    <BrowserRouter> 
-    <Route path="/login"  component={LoginSignUpLinkPage} />
+    <Route path="/login"  component={AuthForm} />
     {/* <Route path="/index2" component={Greetings} /> */}
-    <Route path="/index43" component={Greetings} />
-    <Route path="/signup" component={SignupPage} />
+    {/* <Route path="/index43" component={Greetings} />
+    <Route path="/signup" component={SignupPage} /> */}
   </BrowserRouter>
     
   </Provider>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ var path = require("path");
 const webpack = require("webpack");
 
 // eslint-disable-next-line no-unused-vars
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {  
+  
+  
   devtool: "source-map",
   entry: {
     // vendor: ["@material-ui/styles"],
     main: path.join(__dirname, "src/js/index.js")
   },
-  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
+  // plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     // filename: "[name].js",
     filename: "main.js",
@@ -18,17 +20,32 @@ module.exports = {
   },
 
   module: {
-    rules: [
+    rules: [      
+      {
+        test: /\.svg/,
+        use: {
+            loader: 'svg-url-loader',
+            options: {}
+        }
+    },
+       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader'        
+      },
       {
         test: /\.css$/,
         loader: 'style-loader'
-      }, {
-        test: /\.css$/,
-        loader: 'css-loader',
-        query: {
-          modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
-        }
       },
       {
         test: /\.(js|jsx)$/,
@@ -40,20 +57,7 @@ module.exports = {
             sourceMap: true
           }
         }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-
-        use: ["style-loader", "css-loader"]
-      }
+      }     
     ]
   }
 };
